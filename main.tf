49fb5a5def11c89332ab48d0f69df0f829cac121
dani <dani.com>
2021-03-26 12:09:13 +0500

    Comment Added

--- main.tf
+++ main.tf
@@ -1,12 +1,15 @@
+# Provider AWS
 provider "aws" {
   region = "us-east-1"
 }
+# AWS VPC
 resource "aws_vpc" "my_app" {
   cidr_block = var.vpc_cidr_block
   tags = {
     Name = "${var.env_prefix}-vpc"
   }
 }
+# AWS Subnet
 resource "aws_subnet" "subnet-1" {
   vpc_id = aws_vpc.my_app.id
   cidr_block = var.subnet_cidr_block
@@ -14,12 +17,14 @@ resource "aws_subnet" "subnet-1" {
     Name = "${var.env_prefix}-subnet"
   }
 }
+#AWS IGW
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.my_app.id
   tags = {
       Name = "${var.env_prefix}-igw"
   }
 }
+# AWS RT
 resource "aws_route_table" "my_app_route_table" {
   vpc_id = aws_vpc.my_app.id
   route {
